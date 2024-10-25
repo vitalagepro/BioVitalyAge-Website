d43c2dc7e5d962e75f387b0604061fa9abfaef92
vito di venosa <110898637+Vito290500@users.noreply.github.com>
2024-10-25 14:42:53 +0200

    push delle modifiche del 25/10

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const navbar = document.getElementById('navbar');
 const HomeContainer = document.getElementById('Home_Section');
-console.log(HomeContainer)
 
 
 window.onscroll = function() {
@@ -17,5 +16,73 @@ window.onscroll = function() {
         }
 };
 
+document.querySelectorAll('.accordion-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        const isActive = this.classList.contains('active');
+
+        // Remove 'active' class from all buttons
+        document.querySelectorAll('.accordion-btn').forEach(btn => {
+            btn.classList.remove('active');
+        });
+
+        // If the button was not active before, add 'active', otherwise leave it closed
+        if (!isActive) {
+            this.classList.add('active');
+        }
+    });
+});
 
 
+
+
+const allh3 = document.querySelectorAll('h3');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.animation = 'fade-in 2s forwards';
+        }
+    });
+}, {
+    threshold: 0.1 
+});
+
+allh3.forEach(h3 => {
+    observer.observe(h3);
+});
+
+
+const allImgToTranslate = document.querySelectorAll('.translate-img');
+
+const translator = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.animation = 'translate 2s forwards';
+            translator.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.1 
+});
+
+allImgToTranslate.forEach(img => {
+    translator.observe(img); 
+});
+
+
+const ImgToScale = document.querySelectorAll('.scale-img');
+
+const scalator = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scale-animate');
+            scalator.unobserve(entry.target); // Stop observing after it has scaled
+        }  
+    });
+}, {
+    threshold: 0.1 
+});
+
+ImgToScale.forEach(img => {
+    scalator.observe(img); 
+});
